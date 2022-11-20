8d18ee6b3b09917f278fb3764dd18be453a779cc
Wektors <wiktorm16@gmail.com>
2022-11-20 08:06:18 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.sass";
+
+function App() {
+	return (
+		<div className="App">
+			<div id="timer-label">21:37</div>
+			<div id="control-label">
+				<div id="break-label" className="control-item">
+					5
+				</div>
+				<div id="session-label" className="control-item">
+					25
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App;
