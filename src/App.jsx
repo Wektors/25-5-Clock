e66937ab87281f2e0bb38c3711279daf1bef22d7
Wektors <wiktorm16@gmail.com>
2022-11-21 16:44:33 +0100

    ready

--- src/App.jsx
+++ src/App.jsx
@@ -2,53 +2,82 @@ import "./App.sass";
 import { useState, useEffect } from "react";
 
 function App() {
+	
 	const [currMode, setCurrMode] = useState("Session");
+
+	const [isNewSession, setIsNewSession] = useState(true);
 
 	const [isActive, setIsActive] = useState(false);
 
-	const [currSessionLength, setCurrSessionLength] = useState(1500);
+	const [currSessionLength, setCurrSessionLength] = useState(25);
 
-	const [currBreakLength, setCurrBreakLength] = useState(300);
+	const [currBreakLength, setCurrBreakLength] = useState(5);
 
-	const [currTime, setCurrTime] = useState(currSessionLength);
+	const [currTime, setCurrTime] = useState(currSessionLength * 60);
+
 
 	useEffect(() => {
 		let interval = null;
-		if (isActive) {
+		if (isActive && currTime > - 1) {
 			interval = setInterval(() => {
 				setCurrTime((currTime) => currTime - 1);
 			}, 1000);
-		} else if (!isActive || currTime == 0) {
+		} else if (!isActive) {
+			clearInterval(interval);
+		} else if (currTime == -1 && currMode == "Session") {
+			playBeep();
+			setCurrMode("Break");
+			setCurrTime(currBreakLength * 60);
+			clearInterval(interval);
+		} else if (currTime == -1 && currMode == "Break") {
+			playBeep();
+			setCurrMode("Session");
+			setCurrTime(currSessionLength * 60);
 			clearInterval(interval);
 		}
 		return () => clearInterval(interval);
 	}, [isActive, currTime]);
 
 	function toggleActivity() {
+		if (isNewSession) {
+			setCurrTime(currSessionLength * 60);
+			setIsNewSession(false);
+		}
 		setIsActive(!isActive);
 	}
 
-	function reset() {
-		setCurrTime(currSessionLength);
+	function toggleDefault() {
+		setCurrMode("Session");
+		setCurrSessionLength(25);
+		setCurrBreakLength(5);
+		setCurrTime(currSessionLength * 60);
 		setIsActive(false);
+		stopBeep();
+		setIsNewSession(true);
 	}
 
 	function sessionIncr() {
-		setCurrSessionLength(currSessionLength + 60);
-		setCurrTime(currSessionLength);
+		if (currSessionLength < 60) {
+			setCurrSessionLength(currSessionLength + 1);
+		}
 	}
 
 	function sessionDecr() {
-		setCurrSessionLength(currSessionLength - 60);
-		setCurrTime(currSessionLength);
+		if (currSessionLength > 1) {
+			setCurrSessionLength(currSessionLength - 1);
+		}
 	}
 
 	function breakIncr() {
-		setCurrBreakLength(currBreakLength + 60);
+		if (currBreakLength < 60) {
+			setCurrBreakLength(currBreakLength + 1);
+		}
 	}
 
 	function breakDecr() {
-		setCurrBreakLength(currBreakLength - 60);
+		if (currBreakLength > 1) {
+			setCurrBreakLength(currBreakLength - 1);
+		}
 	}
 
 	function toFullTime(seconds) {
@@ -57,28 +86,27 @@ function App() {
 			seconds = seconds - 60;
 			minutes++;
 		}
-
-		let minutesToDisplay = minutes;
-
+		let minutesToDisplay = minutes.toString()
+		let secondsToDisplay = seconds.toString();
 		if (minutes < 10) {
-			minutesToDisplay = "0" + minutes;
-		}
-
-		let secondsToDisplay = seconds;
-
-		if (seconds < 10) {
-			secondsToDisplay = "0" + seconds;
+			minutesToDisplay = "0" + minutesToDisplay
+		} if (seconds < 10) {
+			secondsToDisplay = "0" + secondsToDisplay
 		}
 		return minutesToDisplay + ":" + secondsToDisplay;
+	
 	}
 
-	function toMinutes(seconds) {
-		let minutes = 0;
-		while (seconds > 0) {
-			seconds = seconds - 60;
-			minutes++;
-		}
-		return minutes;
+	function playBeep() {
+		let beep = document.getElementById("beep");
+		beep.volume = 0.2;
+		beep.play();
+	}
+
+	function stopBeep() {
+		let beep = document.getElementById("beep");
+		beep.pause();
+		beep.load();
 	}
 
 	return (
@@ -86,15 +114,16 @@ function App() {
 			<section id="timer-section">
 				<div id="timer-label">
 					{currMode}
-					{isActive}
 				</div>
 				<div id="time-left">{toFullTime(currTime)}</div>
-				<button id="start_stop" onClick={toggleActivity}>
-					{isActive ? <img src="pause.png" /> : <img src="play.png" />}
-					<button id="reset" onClick={reset}>
+				<div>
+					<button id="start_stop" onClick={toggleActivity}>
+						{isActive ? <img src="pause.png" /> : <img src="play.png" />}
+					</button>
+					<button id="reset" onClick={toggleDefault}>
 						<img src="replay.png" />
 					</button>
-				</button>
+				</div>
 			</section>
 			<section id="control-section">
 				<section className="control-item">
@@ -109,7 +138,7 @@ function App() {
 						>
 							+
 						</button>
-						<div id="session-length">{toMinutes(currSessionLength)}</div>
+						<div id="session-length">{currSessionLength}</div>
 						<button
 							id="session-decrement"
 							className="length-control-button"
@@ -131,7 +160,7 @@ function App() {
 						>
 							+
 						</button>
-						<div id="break-length">{toMinutes(currBreakLength)}</div>
+						<div id="break-length">{currBreakLength}</div>
 						<button
 							id="break-decrement"
 							className="length-control-button"
@@ -142,6 +171,7 @@ function App() {
 					</div>
 				</section>
 			</section>
+			<audio id="beep" src="beep.wav"></audio>
 		</div>
 	);
 }
